9bbd13ccea7ab7b27b12144096cb449c13adf1b3
simon lacote <simonlacote@hotmail.fr>
2021-02-25 20:07:41 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :cocktails do
-  resources :doses, only: [:new, :create]
+  resources :doses, only: [:new, :create, :destroy]
 end
 
 resources :ingredients, only: [:index, :show]
-
 
 end
